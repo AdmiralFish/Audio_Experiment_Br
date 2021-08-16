d84a46ab441bff4b128b55b4ec663d38bb2987e8
Tom Peney <tompeney@hotmail.co.uk>
2021-08-16 11:45:45 +0100

    Updated RTdata handler

--- data_handler - times.py
+++ data_handler - times.py
@@ -20,20 +20,15 @@ headers.append('cue_length')
 
 headers.append("s_valid_mean") 
 headers.append("s_valid_congruent_mean") 
+headers.append("s_valid_incongruent_mean")
 headers.append("s_invalid_mean")
 
 headers.append("l_valid_mean") 
 headers.append("l_valid_congruent_mean") 
+headers.append("l_valid_incongruent_mean")
 headers.append("l_invalid_mean")
 
-
-s_valid = []
-s_valid_congruent = []
-s_invalid = []
-
-l_valid = []
-l_valid_congruent = []
-l_invalid = []
+blanks = 0
 
 # Init Master Dataframe
 data_export = pd.DataFrame(columns=headers)
@@ -41,6 +36,16 @@ data_export = pd.DataFrame(columns=headers)
 # Main Loop
 for csv_file in raw_data:
     df = pd.read_csv(csv_file)
+
+    s_valid = []
+    s_valid_congruent = []
+    s_valid_incongruent = []
+    s_invalid = []
+
+    l_valid = []
+    l_valid_congruent = []
+    l_valid_incongruent = []
+    l_invalid = []
 
     # Creates empty dic for storing info on each condition 
     short = {'cue_length': "short"}
@@ -67,10 +72,12 @@ for csv_file in raw_data:
 
     # Collect Response Data
     responses = df['response']
+    cueTarget = df['cueTarget']
 
     # Record responses
     short_responses = responses[0:68]
     long_responses = responses[68:136]
+
 
     # Collect Response Time Data
     response_times = df['response.time']
@@ -79,19 +86,20 @@ for csv_file in raw_data:
     long_response_times = response_times[68:136] # Long Cuetime Condition
 
     # Add short times 
-    counter = 0
     keys = short_response_times.keys()
     for i in range(0,len(short_response_times)):
-        if counter == 4:
-            counter = 0
-        counter += 1
 
-        if counter == 1:
+        if cueTarget[keys[i]] == "shortValid":
             if short_responses[keys[i]] == "response_1":
                 s_valid_congruent.append(short_response_times[keys[i]]) # Congruent SV
-        if counter == 2:
+            elif short_responses[keys[i]] == "response_2":
+                s_valid_incongruent.append(short_response_times[keys[i]]) # Incongruent SV
+
+        if cueTarget[keys[i]] == "longValid":
             if short_responses[keys[i]] == "response_2":
                 s_valid_congruent.append(short_response_times[keys[i]]) # Congruent LV
+            elif short_responses[keys[i]] == "response_1":
+                s_valid_incongruent.append(short_response_times[keys[i]]) # Congruent LV
 
         if short_responses[keys[i]] in ["response_1", "response_2"]: # Valid
             s_valid.append(short_response_times[keys[i]])
@@ -99,33 +107,35 @@ for csv_file in raw_data:
         elif short_responses[keys[i]] in ["response_3", "response_4"]: # Invalid 
             s_invalid.append(short_response_times[keys[i]])
         else:
-            print("Error in short_response_time loop")
+            blanks += 1
     
     # Add long times 
-    counter = 0
     keys = long_response_times.keys()
-    for i in range(0,len(long_response_times)):
-        if counter == 4:
-            counter = 0
-        counter += 1
+    for i in range(0,len(long_response_times)):   
 
-        if counter == 1:
+        if cueTarget[keys[i]] == "shortValid":
             if long_responses[keys[i]] == "response_1":
-                l_valid_congruent.append(long_response_times[keys[i]]) # Congruent SV
-        if counter == 2:
+                l_valid_congruent.append(long_response_times[keys[i]]) # Congruent LV
+            elif long_responses[keys[i]] == "response_2":
+                l_valid_incongruent.append(long_response_times[keys[i]]) # inCongruent LV
+
+        if cueTarget[keys[i]] == "longValid":
             if long_responses[keys[i]] == "response_2":
                 l_valid_congruent.append(long_response_times[keys[i]]) # Congruent LV
+            elif long_responses[keys[i]] == "response_1":
+                l_valid_incongruent.append(long_response_times[keys[i]]) # inCongruent LV
 
         if long_responses[keys[i]] in ["response_1", "response_2"]: # Valid
             l_valid.append(long_response_times[keys[i]])
         elif long_responses[keys[i]] in ["response_3", "response_4"]: # Invalid 
             l_invalid.append(long_response_times[keys[i]])
         else:
-            print("Error in long response time loop")
-    
+            blanks += 1
+
     # Calculate means
     s_valid_mean = statistics.mean(s_valid)
     s_valid_congruent_mean = statistics.mean(s_valid_congruent)
+    s_valid_incongruent_mean = statistics.mean(s_valid_incongruent)
     if len(s_invalid) == 0:
         s_invalid_mean = "N/A"
     else:
@@ -133,6 +143,7 @@ for csv_file in raw_data:
     
     l_valid_mean = statistics.mean(l_valid)
     l_valid_congruent_mean = statistics.mean(l_valid_congruent)
+    l_valid_incongruent_mean = statistics.mean(l_valid_incongruent)
     if len(l_invalid) == 0:
         l_invalid_mean = "N/A"
     else:
@@ -141,12 +152,13 @@ for csv_file in raw_data:
     # Add Response Data to Dict
     short["s_valid_mean"] = s_valid_mean
     short["s_valid_congruent_mean"] = s_valid_congruent_mean
+    short["s_valid_incongruent_mean"] = s_valid_incongruent_mean
     short["s_invalid_mean"] = s_invalid_mean
 
     long["l_valid_mean"] = l_valid_mean
     long["l_valid_congruent_mean"] = l_valid_congruent_mean
+    long["l_valid_incongruent_mean"] = l_valid_incongruent_mean
     long["l_invalid_mean"] = l_invalid_mean
-
 
     # Append Data to master Dataframe 
     data_export = data_export.append(short, ignore_index=True)
@@ -154,3 +166,5 @@ for csv_file in raw_data:
 
 # Export to Excel
 data_export.to_excel(f"{cwd}\data_export_times.xlsx", sheet_name="raw_data", index=False, header=True)
+
+print("blanks = ", blanks)
